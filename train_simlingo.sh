f809747937ff3188c65f9f1b0d976ef25d6cc8fa
pim1yh_bosch <Michele.Pierallini@bosch.com>
2025-12-11 14:31:32 +0900

    Add train_simlingo.sh helper

--- train_simlingo.sh
+++ train_simlingo.sh
@@ -12,4 +12,4 @@ export NCCL_DEBUG=INFO
 
 export OMP_NUM_THREADS=64 # Limits pytorch to spawn at most num cpus cores threads
 export OPENBLAS_NUM_THREADS=1  # Shuts off numpy multithreading, to avoid threads spawning other threads.
-WANDB__SERVICE_WAIT=10 python simlingo_base_training/train.py experiment=simlingo_seed1 data_module.batch_size=8 gpus=1 name=simlingo_seed1
+WANDB__SERVICE_WAIT=10 python simlingo_training/train.py experiment=simlingo_seed1 data_module.batch_size=8 gpus=1 name=simlingo_seed1
